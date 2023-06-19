1e70206db3b5e24a6954e442010213217f87f2ca
jungAh.Han <truth.h406@gmail.com>
2023-06-19 09:51:53 +0900

    🎉 setting project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var numberDisplay = document.getElementById('number-display');
+var number = 0;
+
+function increaseNumber(increment) {
+  var newNumber = number + increment;
+  if (newNumber <= 999) {
+    number = newNumber;
+    numberDisplay.textContent = number.toString().padStart(3, '0');
+  }
+}
+
+function decreaseNumber(decrement) {
+  var newNumber = number - decrement;
+  if (newNumber >= 0) {
+    number = newNumber;
+    numberDisplay.textContent = number.toString().padStart(3, '0');
+  }
+}
+
+function resetNumber() {
+  number = 0;
+  numberDisplay.textContent = number.toString().padStart(3, '0');
+}
